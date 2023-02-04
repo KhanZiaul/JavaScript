df9714efca138c84a796a2209118ed45d467ce48
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-05 01:02:03 +0600

    done

---
+++ Array,String,Loop,Function,Object/MODULE-8/problem-5.js
@@ -0,0 +1,12 @@
+// radian to degree
+
+function radianToDegree(value){
+
+    let radianToDegree = value * 57.2958;
+
+    return radianToDegree ;
+}
+
+let radianToDegreeIs = radianToDegree(10).toFixed(2);
+
+console.log(radianToDegreeIs)
