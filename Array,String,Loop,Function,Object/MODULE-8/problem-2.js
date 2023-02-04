333f60f5390f3c765b88a0b8e805286c43ef7203
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-04 22:14:16 +0600

    problem-2 done

---
+++ Array,String,Loop,Function,Object/MODULE-8/problem-2.js
@@ -0,0 +1,11 @@
+//  Multiplications of 3
+
+
+let number = 3;
+
+for(let i=1; i <= 10; i++){
+
+    let multiplications = number * i ;
+    
+    console.log(i + ' * ' + 3  + ' = ' + multiplications);
+}
