a692a8120ce499b30815cba0ad65fb387d27f0e9
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-05 13:12:35 +0600

    done

---
+++ Array,String,Loop,Function,Object/MODULE-8/problem8.js
@@ -0,0 +1,41 @@
+// public bus fare
+
+function publicBusFare(public){
+
+if(public !== 'number' || public < 50){
+
+    console.log("provide valid participants");
+}
+
+else if(public >= 50 && public % 50 === 0){
+
+    console.log('Need No Extra amount');
+}
+
+else if(public >= 50 && public % 50 % 11 === 0){
+
+    console.log('Need No Extra amount');
+}
+
+else if (public >= 50 && public % 50 % 11 !== 0 ){
+
+    let amount = public % 50 % 11 * 250 ;
+
+    console.log(amount , 'taka');
+}
+
+else if (public >= 11 && public < 50 && 11 % public === 0 ){
+
+    console.log('Need No Extra amount');
+}
+
+else if (public >= 11 && public < 50 && 11 % public !== 0 ){
+
+    let amount = public % 11 * 250 ;
+
+    console.log(amount , 'taka');
+}
+
+}
+
+publicBusFare('auhax');
