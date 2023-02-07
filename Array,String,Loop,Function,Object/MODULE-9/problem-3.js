1b4e49fabfcef02c98ae66892e6dbb4e5e4b896c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-08 00:05:37 +0600

    problem-3 done

---
+++ Array,String,Loop,Function,Object/MODULE-9/problem-3.js
@@ -0,0 +1,12 @@
+// array like object
+
+function values(num1,num2){
+
+    console.log(num1,num2);
+    
+    console.log(arguments);
+
+    console.log(arguments[3]);
+}
+
+values(10,20,30,40,50,60);
