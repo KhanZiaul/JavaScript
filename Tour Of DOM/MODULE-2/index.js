3a0dcd6ddbac864d45f390d2cbccfb2a96d355fc
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-10 20:18:31 +0600

    event-handeler-3 done

--- Tour Of DOM/MODULE-2/index.js
+++ Tour Of DOM/MODULE-2/index.js
@@ -6,3 +6,23 @@ function makeRed(){
 
     document.body.style.backgroundColor = 'red';
 }
+
+// option-3 :
+
+// const makeBlueButton = document.getElementById('makeBlue');
+
+// makeBlueButton.onclick = makeBlue;
+
+// function makeBlue(){
+
+//     document.body.style.backgroundColor = 'blue';
+// }
+
+// Another way of option-3 :
+
+const makeBlueButton = document.getElementById('makePurple');
+
+makeBlueButton.onclick = function makePurple(){
+
+    document.body.style.backgroundColor = 'purple';
+}
