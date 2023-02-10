2bd314a163d5ea5e417df4d8e2945ace6ab6eb5f
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-10 20:24:12 +0600

    updated

--- Tour Of DOM/MODULE-2/index.js
+++ Tour Of DOM/MODULE-2/index.js
@@ -1,6 +1,12 @@
 // EVENT HANDELER
 
-// option-2 :
+//option-1 : directly set on the html element--------------->
+
+/* <h2 onclick="console.log('eh')">JavaScript Events</h2>
+    
+<button style="display: block; margin-bottom: 10px;" title="click me" onclick="console.log('oh shit! you clicked me') ">Click Me</button> */
+
+// option-2 : add onclick function on the html element-------------->
 
 function makeRed(){
 
@@ -8,6 +14,8 @@ function makeRed(){
 }
 
 // option-3 :
+
+// first way of option -3 : 
 
 // const makeBlueButton = document.getElementById('makeBlue');
 
@@ -26,3 +34,5 @@ makeBlueButton.onclick = function makePurple(){
 
     document.body.style.backgroundColor = 'purple';
 }
+
+// option-4 : 
