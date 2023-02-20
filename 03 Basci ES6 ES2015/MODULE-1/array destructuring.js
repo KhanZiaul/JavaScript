47e7f188db90adb9da6967da6bc7ddad887a2beb
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-20 23:44:24 +0600

    array destructuring

---
+++ 03 Basci ES6 ES2015/MODULE-1/array destructuring.js
@@ -0,0 +1,18 @@
+// JavaScript array destructuring
+
+const ages = [10,20,30,40];
+
+const [young,semiYoug,olde,older]= ages;
+
+console.log(older);
+
+// another example by function
+
+function character(){
+
+    return ['khanZia' , 'sebuMohi'];
+}
+
+const [semiGood,altraGood] = character();
+
+console.log(altraGood);
