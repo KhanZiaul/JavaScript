d246a94ec4652dcb83181d3f6dba2cf066f6d34f
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-20 23:29:13 +0600

    object destructuring done

---
+++ 03 Basci ES6 ES2015/MODULE-1/object destruturing.js
@@ -0,0 +1,17 @@
+// JavaScript object destructuring
+
+const palyer = {
+        girlName: 'sundori', 
+        address: 'doulodia',
+        score : 150
+    }
+
+const {girlName,address,score} = palyer;
+
+console.log(girlName);
+
+console.log(address);
+
+console.log(score);
+
+console.log(girlName);
