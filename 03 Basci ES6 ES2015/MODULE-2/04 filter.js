56c3906bbde9ebec92098736e72d8546b27848b6
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-21 19:43:29 +0600

    filter done

---
+++ 03 Basci ES6 ES2015/MODULE-2/04 filter.js
@@ -0,0 +1,22 @@
+// array filter method
+
+const ages = [10,40,55,20,77];
+
+const older = ages.filter((age) => age > 25);
+
+console.log(older);
+
+const products = [
+    {id:1, name:'laptop',price:45000},
+
+    {id:2, name:'mobile',price:80000},
+
+    {id:3, name:'tab',price:8000}
+];
+
+const highPrice = products.filter((product)=>{
+
+    return product.price > 10000;
+});
+
+console.log(highPrice);
