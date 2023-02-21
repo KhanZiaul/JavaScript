03861f5ebe9dee306aceec7c2b38309479741ac0
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-21 19:47:36 +0600

    find done

--- 03 Basci ES6 ES2015/MODULE-2/05. find.js
+++ 03 Basci ES6 ES2015/MODULE-2/05. find.js
@@ -1,2 +1,24 @@
 // array find method
 
+// array filter method
+
+const ages = [10,40,55,20,77];
+
+const older = ages.find((age) => age > 25);
+
+console.log(older);
+
+const products = [
+    {id:1, name:'laptop',price:45000},
+
+    {id:2, name:'mobile',price:80000},
+
+    {id:3, name:'tab',price:8000}
+];
+
+const highPrice = products.find((product)=>{
+
+    return product.price > 10000;
+});
+
+console.log(highPrice);
