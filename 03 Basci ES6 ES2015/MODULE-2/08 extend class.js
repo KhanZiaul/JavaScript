5451b54267f8096fdf7f82dc5a416254238e29c7
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-22 00:26:46 +0600

    class extends added

--- 03 Basci ES6 ES2015/MODULE-2/08 extend class.js
+++ 03 Basci ES6 ES2015/MODULE-2/08 extend class.js
@@ -1,2 +1,52 @@
 // Javascript extend class
 
+class Information{
+
+    name;
+
+    location;
+
+    constructor(name,location){
+
+        this.name = name;
+
+        this.location = location;
+    }
+
+    feedback(){
+
+        console.group(`${this.name}, thanks for your feedback`);
+    }
+};
+
+class Developer extends Information{
+
+    job = 'developer';
+    
+    constructor(name,location){
+
+        super(name,location);
+    }
+
+};
+
+class supporter extends Information{
+
+    job = 'supporter';
+
+    constructor(name,location){
+
+        super(name,location);
+    }
+
+};
+
+const megha = new Developer('Megha','Mohammadpur');
+
+console.log(megha);
+
+const location = megha.location;
+
+console.log(location);
+
+megha.feedback();
