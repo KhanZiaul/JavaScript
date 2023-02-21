23157b318e7edb673e4668caceb99ef89d296853
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-21 19:24:27 +0600

    foreach done

---
+++ 03 Basci ES6 ES2015/MODULE-2/03 foreach.js
@@ -0,0 +1,20 @@
+// for each method
+
+const names = ['zia','megha','rumman'];
+
+names.forEach((name)=>{
+
+    console.log(name);
+});
+
+
+const products = [
+    {id:1, name:'laptop',price:45000},
+
+    {id:2, name:'mobile',price:80000}
+];
+
+products.forEach((product)=>{
+    
+    console.log(product);
+})
