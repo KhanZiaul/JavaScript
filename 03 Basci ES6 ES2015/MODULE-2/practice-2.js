51994d62f46c12ac19479fad1693ef51450c7f70
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-22 00:47:45 +0600

    practice-2 done

---
+++ 03 Basci ES6 ES2015/MODULE-2/practice-2.js
@@ -0,0 +1,15 @@
+// array filter method
+
+// const numbers = [33, 50, 79, 78, 90, 101, 30 ];
+
+// const filtering = numbers.filter(number => number%10===0);
+
+// console.log(filtering);
+
+// array find method
+
+const numbers = [33, 50, 79, 78, 90, 101, 30 ];
+
+const finding= numbers.find(number => number%10===0);
+
+console.log(finding);
