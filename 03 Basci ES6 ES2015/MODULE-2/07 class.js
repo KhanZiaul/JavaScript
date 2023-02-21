7aa64f897add3268418da332c3e7770094d0f970
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-21 21:30:06 +0600

    class done

---
+++ 03 Basci ES6 ES2015/MODULE-2/07 class.js
@@ -0,0 +1,27 @@
+// class (syntatic sugar)
+
+class Instructor{
+
+    name;
+    designation = 'web course Instructor'
+    team = 'web team'
+
+    startSupportSession(time){
+        console.log(`start supportSession at ${time}`);
+    }
+
+    location;
+
+    constructor(name,location){
+
+        this.name = name;
+
+        this.location = location;
+    }
+};
+
+const khanZia = new Instructor('Khan Zia','Barishal');
+
+console.log(khanZia);
+
+khanZia.startSupportSession('9.00 PM');
