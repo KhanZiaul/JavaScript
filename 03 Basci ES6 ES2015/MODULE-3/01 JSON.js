ef9f37861e6029d94b7f68b4a1ef262e49e4229c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-22 22:04:32 +0600

    first example done

---
+++ 03 Basci ES6 ES2015/MODULE-3/01 JSON.js
@@ -0,0 +1,18 @@
+// JavaScript Object Notation
+
+// first example
+
+const information = {
+
+    name :'khan zia',
+
+    age : 27,
+
+    job : 'web developer'
+}
+
+const stringified = JSON.stringify(information);
+
+console.log(information);
+
+console.log(stringified);
