4f2e2d09239d012f2f6afe8355ae667f644f0660
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-24 01:29:07 +0600

    done

--- 03 Basci ES6 ES2015/MODULE-4/js/ramdomGirl.js
+++ 03 Basci ES6 ES2015/MODULE-4/js/ramdomGirl.js
@@ -2,11 +2,23 @@ const loadUser = () =>{
 
     fetch('https://randomuser.me/api/?gender=female')
     .then(res => res.json())
-    .then(data => loadUser(data))
+    .then(data => userData(data))
 }
 
 const userData = data => {
-    console.log(data);
+
+    const name = document.getElementById('name');
+
+    name.innerText = data.results[0].name.first;
+
+    const gender = document.getElementById('gender');
+
+    gender.innerText = data.results[0].gender;
+
+    const contact = document.getElementById('phone');
+
+    contact.innerText = data.results[0].phone;
+
 }
 
 loadUser();
