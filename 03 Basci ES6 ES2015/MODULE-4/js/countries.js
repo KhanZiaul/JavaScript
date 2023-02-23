7402a38e93c58da8fcc0060748b877e990c63aa8
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-24 04:08:06 +0600

    countries updated

--- 03 Basci ES6 ES2015/MODULE-4/js/countries.js
+++ 03 Basci ES6 ES2015/MODULE-4/js/countries.js
@@ -10,11 +10,11 @@ function showdata(data){
 
     const countries = document.getElementById('countries');
 
-   data.forEach(country => {
-
-    console.log(country);
+    data.forEach(country => {
 
     const countryDiv = document.createElement('div');
+
+    countryDiv.classList.add('styles')
 
     countryDiv.innerHTML =
     `
@@ -23,11 +23,34 @@ function showdata(data){
     <h4> Region : ${country.region}</h4>
 
     <h4> Populations : ${country.population} </h4>
+
+    <button onclick= "countryDetails('${country.cca2}')"> Deatils </button>
     `;
 
     countries.appendChild(countryDiv);
     
    });
+}
+
+function countryDetails(code){
+
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+
+    fetch(url)
+    .then(res => res.json())
+    .then(details => showdetails(details[0]))
+}
+
+function showdetails(details){
+
+    const countryDetails = document.getElementById('country-details');
+
+    countryDetails.innerHTML =
+    `
+    <h2 style="color: rgb(243, 242, 242); font-size: 30px;">${details.name.common} </h2>
+
+    <img src="${details.flags.png}" alt="">
+    `;
 }
 
 loadData();
