31da12c08561824b5714d213fa291f6a7537bfa6
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-24 01:40:54 +0600

    style added and updated

--- 03 Basci ES6 ES2015/MODULE-4/js/ramdomGirl.js
+++ 03 Basci ES6 ES2015/MODULE-4/js/ramdomGirl.js
@@ -7,9 +7,13 @@ const loadUser = () =>{
 
 const userData = data => {
 
+    const image = document.getElementById('image');
+
+    image.setAttribute('src',data.results[0].picture.large);
+
     const name = document.getElementById('name');
 
-    name.innerText = data.results[0].name.first;
+    name.innerText = data.results[0].name.first + ' ' + data.results[0].name.last;
 
     const gender = document.getElementById('gender');
 
