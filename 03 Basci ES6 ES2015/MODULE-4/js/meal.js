b6a63769230fb94d92a9da5c7d116535807a8ad4
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-24 15:44:15 +0600

    meal html updated

---
+++ 03 Basci ES6 ES2015/MODULE-4/js/meal.js
@@ -0,0 +1,50 @@
+const loadmeals = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+    .then(res => res.json())
+    .then(mealdata => showData(mealdata))
+}
+
+function showData(mealdata){
+
+    const showMeal= document.getElementById('showMeal');
+
+    const meals = mealdata.meals;
+
+    meals.map((meal)=>{
+
+        const mealDiv = document.createElement('div');
+
+        mealDiv.classList.add('rounded-xl');
+
+        mealDiv.classList.add('bg-sky-100');
+
+        mealDiv.classList.add('mb-4');
+
+        mealDiv.innerHTML =
+        `
+       <figure class="px-10 pt-10">
+        <img src="${meal.strMealThumb}" alt="Shoes" class="rounded-xl" />
+      </figure>
+        <div class="card-body items-center text-center">
+           <h2 class="card-title">${meal.strMeal}</h2>
+           <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions">
+                <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+
+        `;
+
+        showMeal.appendChild(mealDiv);
+
+        console.log(meal);
+    });
+
+}
+
+document.getElementById('search-btn').addEventListener('click',function(){
+
+    const 
+});
+
+loadmeals();
