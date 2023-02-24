2f24c7373c9664223de7b6e9830af50eae59d023
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-25 03:00:20 +0600

    idmeal button added

--- 03 Basci ES6 ES2015/MODULE-4/js/meal.js
+++ 03 Basci ES6 ES2015/MODULE-4/js/meal.js
@@ -27,16 +27,15 @@ function showData(mealdata){
         mealDiv.innerHTML =
         `
        <figure class="px-10 pt-10">
-        <img src="${meal.strMealThumb}" alt="Shoes" class="rounded-xl" />
+        <img src="${meal.strMealThumb}" alt="meal" class="rounded-xl" />
       </figure>
         <div class="card-body items-center text-center">
            <h2 class="card-title">${meal.strMeal}</h2>
            <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions">
-                <button class="btn btn-primary">Buy Now</button>
+        <div class="card-actions"> 
+        <a onclick="mealDetails(${meal.idMeal})" href="#my-modal-2" class="btn">Details</a>      
           </div>
         </div>
-
         `;
 
         showMeal.appendChild(mealDiv);
@@ -55,3 +54,8 @@ document.getElementById('search-btn').addEventListener('click',function(){
     document.getElementById('search-input').value = "";
 
 });
+
+const mealDetails = (idMeal) =>{
+
+    console.log(idMeal);
+};
