e957b1afdd6f10bcb2ef12388f4abe1edfd7e939
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-25 16:10:43 +0600

    app-1-data done

--- 03 Basci ES6 ES2015/MODULE-5/api-1-data.js
+++ 03 Basci ES6 ES2015/MODULE-5/api-1-data.js
@@ -1,35 +1,53 @@
-const person = {
-    found: 2,
-    message: "Found 2 persons",
-    result: [
-      {
-        name: {
-          common: "John",
-          fullName: ["John", "Doe"]
-        },
-        age: 32,
-        isMale: false,
-        address: {
-          street: "13/A St Joseph",
-          house: 10,
-        },
+const persons = {
+  found: 2,
+  message: "Found 2 persons",
+  result: [
+    {
+      name: {
+        common: "John",
+        fullName: ["John" + " " + "Doe"]
       },
-      {
-        name: {
-          common: "Humayoun",
-          fullName: ["Humayoun", "Kabir"]
-        },
-        age: 33,
-        isMale: false,
-        address: {
-          street: "13/A St Lucia",
-          house: 11,
-        },
+      age: 32,
+      isMale: false,
+      address: {
+        street: "13/A St Joseph",
+        house: 10,
       },
-    ]
-  };
+    },
+    {
+      name: {
+        common: "Humayoun",
+        fullName: ["Humayoun" + " " + "Kabir"]
+      },
+      age: 33,
+      isMale: false,
+      address: {
+        street: "13/A St Lucia",
+        house: 11,
+      },
+    },
+  ]
+};
 
 
-  console.log(person)
 
-  
+persons.result.map((person) => {
+
+  const parentDiv = document.getElementById('person');
+
+  const childDiv = document.createElement('div');
+
+  childDiv.classList.add('mb-5')
+
+  childDiv.innerHTML =
+  `
+  <p>Person Name : ${person.name.fullName}</p>
+
+  <p> Age : ${person.age}</p>
+
+  <p> Street : ${person.address.street}</p>
+  `;
+
+  parentDiv.appendChild(childDiv);
+});
+
