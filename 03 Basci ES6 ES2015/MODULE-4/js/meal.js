4196d54319ee84beba74cabedc6d2db68b57febe
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-25 23:03:13 +0600

    updated

--- 03 Basci ES6 ES2015/MODULE-4/js/meal.js
+++ 03 Basci ES6 ES2015/MODULE-4/js/meal.js
@@ -1,10 +1,13 @@
 let searchText = "";
 
 const loadmeals = (search) => {
+
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
 
     fetch(url)
+
         .then(res => res.json())
+
         .then(mealdata => showData(mealdata, 6))
 }
 
@@ -41,15 +44,24 @@ function showData(mealdata, limit) {
 
         mealDiv.innerHTML =
             `
-           <figure class="px-10 pt-10">
-            <img src="${meal.strMealThumb}" alt="meal" class="rounded-xl" />
-          </figure>
+            <figure class="px-10 pt-10">
+
+               <img src="${meal.strMealThumb}" alt="meal" class="rounded-xl" />
+
+            </figure>
+
             <div class="card-body items-center text-center">
+
                <h2 class="card-title">${meal.strMeal}</h2>
+
                <p>If a dog chews shoes whose shoes does he choose?</p>
+
             <div class="card-actions"> 
-            <a onclick="mealDetails(${meal.idMeal})" href="#my-modal-2" class="btn">Details</a>      
+
+            <a onclick="mealDetails(${meal.idMeal})" href="#my-modal-2" class="btn">Details</a>   
+
               </div>
+              
             </div>
             `;
 
@@ -64,18 +76,20 @@ function showMore(){
 
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
 
-    fetch(url)
+        fetch(url)
+
         .then(res => res.json())
+
         .then(mealdata => showData(mealdata, 0))
-
-
 }
 
 
 document.getElementById('search-btn').addEventListener('click', function () {
 
     const searchInput = document.getElementById('search-input').value;
+
     searchText = searchInput
+
     loadmeals(searchInput);
 
     document.getElementById('search-input').value = "";
@@ -85,9 +99,13 @@ document.getElementById('search-btn').addEventListener('click', function () {
 const mealDetails = (idMeal) => {
 
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+
     fetch(url)
+
         .then(res => res.json())
+
         .then(details => mealData(details))
+
         .catch(error => {
             console.log(error);
         });
