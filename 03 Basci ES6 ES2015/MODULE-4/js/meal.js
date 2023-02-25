c0fbbf88e1b19433677da666687dc6f336cee912
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-25 15:43:46 +0600

    api-1-data file added

--- 03 Basci ES6 ES2015/MODULE-4/js/meal.js
+++ 03 Basci ES6 ES2015/MODULE-4/js/meal.js
@@ -1,20 +1,42 @@
 const loadmeals = (search) => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
 
-     fetch(url)
-    .then(res => res.json())
-    .then(mealdata => showData(mealdata))
+    fetch(url)
+        .then(res => res.json())
+        .then(mealdata => showData(mealdata, 6))
 }
 
-function showData(mealdata){
+// ---------------------------
 
-    const showMeal= document.getElementById('showMeal');
+function showData(mealdata, limit) {
+
+    const showMeal = document.getElementById('showMeal');
 
     showMeal.innerHTML = '';
 
-    const meals = mealdata.meals;
+    let meals = mealdata.meals;
 
-    meals.map((meal)=>{
+    console.log(mealdata);
+
+    if (limit) {
+
+        meals = mealdata?.meals.slice(0, limit);
+
+        const seeMorebtn = document.getElementById('seeMore-btn');
+
+        seeMorebtn.classList.remove('hidden');
+    }
+
+    if(limit) {
+
+        document.getElementById('seeMore-btn').addEventListener('click', function(){
+
+            console.log('click');
+        });
+    }
+
+    meals.map((meal) => {
+
 
         const mealDiv = document.createElement('div');
 
@@ -25,27 +47,29 @@ function showData(mealdata){
         mealDiv.classList.add('mb-4');
 
         mealDiv.innerHTML =
-        `
-       <figure class="px-10 pt-10">
-        <img src="${meal.strMealThumb}" alt="meal" class="rounded-xl" />
-      </figure>
-        <div class="card-body items-center text-center">
-           <h2 class="card-title">${meal.strMeal}</h2>
-           <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions"> 
-        <a onclick="mealDetails(${meal.idMeal})" href="#my-modal-2" class="btn">Details</a>      
-          </div>
-        </div>
-        `;
+            `
+           <figure class="px-10 pt-10">
+            <img src="${meal.strMealThumb}" alt="meal" class="rounded-xl" />
+          </figure>
+            <div class="card-body items-center text-center">
+               <h2 class="card-title">${meal.strMeal}</h2>
+               <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions"> 
+            <a onclick="mealDetails(${meal.idMeal})" href="#my-modal-2" class="btn">Details</a>      
+              </div>
+            </div>
+            `;
 
         showMeal.appendChild(mealDiv);
 
         console.log(meal);
-    });
 
+    });
 }
 
-document.getElementById('search-btn').addEventListener('click',function(){
+
+
+document.getElementById('search-btn').addEventListener('click', function () {
 
     const searchInput = document.getElementById('search-input').value;
 
@@ -55,21 +79,119 @@ document.getElementById('search-btn').addEventListener('click',function(){
 
 });
 
-const mealDetails = (idMeal) =>{
+const mealDetails = (idMeal) => {
 
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
     fetch(url)
-   .then(res => res.json())
-   .then(details => mealData(details))
-   .catch(error => {
-    console.log(error);
-   });
+        .then(res => res.json())
+        .then(details => mealData(details))
+        .catch(error => {
+            console.log(error);
+        });
 };
 
-function mealData(details){
+function mealData(details) {
 
-document.getElementById('area').innerText = details.meals[0].strArea;
+    document.getElementById('area').innerText = details.meals[0].strArea;
 
-document.getElementById('procedure').innerText = details.meals[0].strInstructions;
+    document.getElementById('procedure').innerText = details.meals[0].strInstructions;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------
+
+
+
+// const loadmeals = (search) => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+
+//      fetch(url)
+//     .then(res => res.json())
+//     .then(mealdata => showData(mealdata))
+// }
+
+// function showData(mealdata){
+
+//     const showMeal= document.getElementById('showMeal');
+
+//     showMeal.innerHTML = '';
+
+//     const meals = mealdata.meals;
+
+//     meals.map((meal)=>{
+
+//         const mealDiv = document.createElement('div');
+
+//         mealDiv.classList.add('rounded-xl');
+
+//         mealDiv.classList.add('bg-sky-100');
+
+//         mealDiv.classList.add('mb-4');
+
+//         mealDiv.innerHTML =
+//         `
+//        <figure class="px-10 pt-10">
+//         <img src="${meal.strMealThumb}" alt="meal" class="rounded-xl" />
+//       </figure>
+//         <div class="card-body items-center text-center">
+//            <h2 class="card-title">${meal.strMeal}</h2>
+//            <p>If a dog chews shoes whose shoes does he choose?</p>
+//         <div class="card-actions">
+//         <a onclick="mealDetails(${meal.idMeal})" href="#my-modal-2" class="btn">Details</a>
+//           </div>
+//         </div>
+//         `;
+
+//         showMeal.appendChild(mealDiv);
+
+//         console.log(meal);
+//     });
+
+// }
+
+// document.getElementById('search-btn').addEventListener('click',function(){
+
+//     const searchInput = document.getElementById('search-input').value;
+
+//     loadmeals(searchInput);
+
+//     document.getElementById('search-input').value = "";
+
+// });
+
+// const mealDetails = (idMeal) =>{
+
+//     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+//     fetch(url)
+//    .then(res => res.json())
+//    .then(details => mealData(details))
+//    .catch(error => {
+//     console.log(error);
+//    });
+// };
+
+// function mealData(details){
+
+// document.getElementById('area').innerText = details.meals[0].strArea;
+
+// document.getElementById('procedure').innerText = details.meals[0].strInstructions;
+
+// }
