ad631ad0d3cc0754a1dfab70b5beff2a0ec8df27
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-27 16:09:43 +0600

    object method done

--- 03 Basci ES6 ES2015/MODULE-6/01 object methods.js
+++ 03 Basci ES6 ES2015/MODULE-6/01 object methods.js
@@ -1,1 +1,22 @@
-// objects method
+// objects method inside method
+
+const student = {
+
+    name:'khan zia',
+    
+    examiner:function(){
+
+        return this.name;
+    },
+
+    improvement:function(subject){
+
+        const studentName = this.examiner();
+
+        return studentName + ` will attend in improvement exam in ${subject}`;
+    }
+}
+
+const info = student.improvement('physics');
+
+console.log(info);
