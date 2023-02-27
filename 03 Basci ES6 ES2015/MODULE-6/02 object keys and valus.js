796dceeda80eea4d986f4a58d8b4861d72c15aac
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-27 16:14:40 +0600

    object keys and valus done

--- 03 Basci ES6 ES2015/MODULE-6/02 object keys and valus.js
+++ 03 Basci ES6 ES2015/MODULE-6/02 object keys and valus.js
@@ -1,2 +1,22 @@
-// object keys and values
+// object keys , values and entries
 
+const student= {
+
+    name:'khan zia',
+
+    age:25,
+
+    isStudent : true
+}
+
+const keys = Object.keys(student);
+
+console.log(keys);
+
+const values = Object.values(student);
+
+console.log(values);
+
+const pair = Object.entries(student);
+
+console.log(pair);
