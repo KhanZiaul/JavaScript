23fcaf1323a741fe76ebab792d120a23bea9cf84
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-27 17:01:40 +0600

    for of loop done

--- 03 Basci ES6 ES2015/MODULE-6/04 for in and for of in object.js
+++ 03 Basci ES6 ES2015/MODULE-6/04 for in and for of in object.js
@@ -1,2 +1,21 @@
 // for in loop and for of loop in object
 
+const bottle = {
+
+    color:'yellow',
+
+    price: 50,
+
+    capacity : 1
+}
+
+// using for of loop
+
+const keys = Object.keys(bottle);
+
+//console.log(keys);
+
+for(const key of keys){
+
+    console.log(key,bottle[key]);
+}
