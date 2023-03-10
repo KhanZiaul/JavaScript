d4053551ad7b443fceeb8012d82a5d925c81bb5a
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-11 02:40:31 +0600

    local storage done

--- 05 Debug/02-local-storage.js
+++ 05 Debug/02-local-storage.js
@@ -5,6 +5,12 @@ function addCart(){
     const itemNumber = document.getElementById('itemNumber').value;
 
     elements(itemName,itemNumber);
+
+    setLocalValue(itemName,itemNumber);
+
+    document.getElementById('itemName').value = '' ;
+
+    document.getElementById('itemNumber').value = '' ;
 }
 
 function elements(itemName,itemNumber){
@@ -16,4 +22,32 @@ function elements(itemName,itemNumber){
     list.innerText = `${itemName} ${itemNumber}`;
 
     parentUl.appendChild(list);
+}
+
+function setLocalValue(itemName,itemNumber){
+
+   let cart={} 
+
+   if(localStorage.getItem('cart')){
+
+    const itemIs= localStorage.getItem('cart');
+    
+    const itemInObj = JSON.parse(itemIs);
+
+    itemInObj[itemName] = itemNumber;
+
+    const itemInJson = JSON.stringify(itemInObj);
+
+    localStorage.setItem('cart',itemInJson)
+
+   }
+
+   else{
+
+    cart[itemName] = itemNumber;
+
+    const cartInJson = JSON.stringify(cart);
+
+    localStorage.setItem('cart',cartInJson);
+   }
 }
