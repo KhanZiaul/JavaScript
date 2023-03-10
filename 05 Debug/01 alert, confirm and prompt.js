04484cdffef86a0792cefbbfed6a331ef485e9ab
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-11 00:40:48 +0600

    alert, confirm and prompt done

--- 05 Debug/01 alert, confirm and prompt.js
+++ 05 Debug/01 alert, confirm and prompt.js
@@ -1,1 +1,42 @@
 // JavaScript alert , confirm and prompt
+
+// alert
+
+function showAlert(){
+
+    alert('it is alert');
+}
+
+// confirm
+
+function showConfirm(){
+    const feedback = confirm('Give me 500 taka');
+
+    if(feedback === true){
+
+        alert('Thanks');
+    }
+
+    else{
+
+        alert('Mor jaiya');
+    }
+}
+
+// prompt
+
+function showPrompt(){
+
+    const feedback = prompt('Enter name');
+
+    if(feedback === null){
+
+        alert('Not allowed here without input')
+    }
+
+    else{
+
+        console.log(feedback)
+        alert(`Welcome ${feedback}`);
+    }
+}
